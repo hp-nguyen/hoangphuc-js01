f84c58d9715b53cf4ce3926cab1d9ad243664e93
HP Nguyen <phucvovinam97@gmail.com>
2023-11-16 00:26:23 +0700

    Redone Ex4.2

--- Ex4/4.2-readNumber.js
+++ Ex4/4.2-readNumber.js
@@ -1,63 +1,57 @@
-function readNumber(number) {
-  const nums = [
-    'không',
-    'một',
-    'hai',
-    'ba',
-    'bốn',
-    'năm',
-    'sáu',
-    'bảy',
-    'tám',
-    'chín',
-    'mười',
-  ];
-  const dividends = [1, 10, 100, 10000];
-  const units = [0, 0, 0, 0];
-  if (number <= 10) return nums[number];
+function readNumber(n) {
+  const originNumber = n;
+  const dividends = [1, 10, 100, 1000, 10000];
+  const texts = ['', ' mươi', ' trăm', ' ngàn', ' vạn'];
+  const units = [0, 0, 0, 0, 0];
+  let result;
+  if (n === 0) return 'không';
+  if (n < 10) return hangDonvi(n);
+  if (n === 10) return 'mười';
+
   for (let i = dividends.length - 1; i >= 0; i--) {
-    const div = Math.floor(number / dividends[i]);
-    if (div >= 1) {
-      units[i] = div;
-      number = number % dividends[i];
+    if (n % dividends[i] === 0 && n === originNumber) {
+      const div = n / dividends[i];
+      return hangChuc(Math.floor(div / 10)) + hangDonvi(div % 10) + texts[i];
     }
-  }
-  const [dv, chuc, ngan, van] = units;
-  const result =
-    hangVan(van) + hangNgan(ngan) + ' ' + hangChuc(chuc) + hangDv(dv);
-  return result.trim();
-}
-function hangChuc(n) {
-  const dividends = [0.1, 1];
-  const units = ['linh', ['mười', 'mươi']];
-  const nums = [
-    'không',
-    'một',
-    'hai',
-    'ba',
-    'bốn',
-    'năm',
-    'sáu',
-    'bảy',
-    'tám',
-    'chín',
-  ];
-  if (n === 0) return units[0];
-  for (let i = dividends.length - 1; i >= 0; i--) {
     const div = Math.floor(n / dividends[i]);
     if (div >= 1) {
-      if (i === 1 && div > 1) {
-        return nums[div] + ' ' + units[i][1];
-      }
-      if (i === 1 && div === 1) {
-        return units[i][0];
-      }
+      units[i] = div;
+      n = n % dividends[i];
     }
   }
+  const [donvi, chuc, tram, ngan, van] = units;
+  if (originNumber < 100) {
+    result = hangChuc(chuc) + ' ' + hangDonvi(donvi);
+  } else {
+    result =
+      hangVan(van) +
+      ' ' +
+      (ngan ? hangNgan(ngan) + ' ' : '') +
+      hangTram(tram) +
+      ' ' +
+      hangChucTram(chuc, donvi) +
+      ' ' +
+      hangDonvi(donvi);
+  }
+  return result.trim();
 }
-function hangDv(n) {
-  const nums = [
-    'không',
+
+console.log(readNumber(0));
+console.log(readNumber(1));
+console.log(readNumber(12));
+console.log(readNumber(1001));
+console.log(readNumber(1000));
+console.log(readNumber(10001));
+console.log(readNumber(12340));
+console.log(readNumber(123456));
+console.log(readNumber(100));
+console.log(readNumber(2000));
+console.log(readNumber(90000));
+console.log(readNumber(900000));
+
+function hangDonvi(n) {
+  const texts = [
+    '',
     'một',
     'hai',
     'ba',
@@ -68,75 +62,53 @@ function hangDv(n) {
     'tám',
     'chín',
   ];
-  return ' ' + nums[n];
+  return texts[n];
+}
+
+function hangChuc(n) {
+  const texts = [
+    '',
+    'một',
+    'hai',
+    'ba',
+    'bốn',
+    'năm',
+    'sáu',
+    'bảy',
+    'tám',
+    'chín',
+  ];
+  if (n >= 2) return texts[n] + ' mươi';
+  if (n === 1) return 'mười';
+  if (n === 0) return '';
+}
+function hangChucTram(chuc, donvi) {
+  const texts = [
+    '',
+    'một',
+    'hai',
+    'ba',
+    'bốn',
+    'năm',
+    'sáu',
+    'bảy',
+    'tám',
+    'chín',
+  ];
+  if (chuc >= 2) return texts[chuc] + ' mươi';
+  if (chuc === 1) return 'mười';
+  if (chuc === 0 && donvi !== 0) return 'linh';
+  return '';
+}
+function hangTram(n) {
+  if (n === 0) return 'không trăm';
+  return hangDonvi(n) + ' trăm';
 }
 function hangNgan(n) {
   if (n === 0) return '';
-  const units = ['trăm', 'ngàn'];
-  const dividends = [1, 10];
-  const nums = [
-    'không',
-    'một',
-    'hai',
-    'ba',
-    'bốn',
-    'năm',
-    'sáu',
-    'bảy',
-    'tám',
-    'chín',
-  ];
-  let result = '';
-  let temp = '';
-  for (let i = dividends.length - 1; i >= 0; i--) {
-    const div = Math.floor(n / dividends[i]);
-    if (div >= 1) {
-      temp = nums[div] + ' ' + units[i];
-      result += temp + ' ';
-      n = n % dividends[i];
-    }
-    temp = '';
-  }
-  return result.trim();
+  return hangDonvi(n) + ' ngàn';
 }
 function hangVan(n) {
-  const dividends = [1, 10];
-  const units = ['', ['mười', 'mươi']];
-  const nums = [
-    'không',
-    'một',
-    'hai',
-    'ba',
-    'bốn',
-    'năm',
-    'sáu',
-    'bảy',
-    'tám',
-    'chín',
-  ];
-  let result = '';
-  let temp = '';
-  if (n === 0) return units[0];
-  for (let i = dividends.length - 1; i >= 0; i--) {
-    const div = Math.floor(n / dividends[i]);
-    if (div >= 1) {
-      if (i === 1 && div > 1) {
-        temp = nums[div] + ' ' + units[i][1] + ' ';
-      } else if (i === 1 && div === 1) {
-        temp = units[i][0];
-      } else temp = nums[div];
-      result += temp;
-      temp = '';
-      n = n % dividends[i];
-    }
-  }
-  return result + ' vạn ';
+  if (n === 0) return '';
+  return hangChuc(Math.floor(n / 10)) + ' ' + hangDonvi(n % 10) + ' vạn';
 }
-
-console.log(readNumber(12));
-console.log(readNumber(6));
-console.log(readNumber(12));
-console.log(readNumber(103));
-console.log(readNumber(76503));
-console.log(readNumber(726503));
-console.log(readNumber(999999));
