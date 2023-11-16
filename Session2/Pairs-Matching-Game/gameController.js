8d26bce491aa11b5afa520e1f353e103e77ea9e4
HP Nguyen <phucvovinam97@gmail.com>
2023-11-16 23:49:02 +0700

    Update assets & Refactory some code of Pair Matching Game

--- Session2/Pairs-Matching-Game/gameController.js
+++ Session2/Pairs-Matching-Game/gameController.js
@@ -5,37 +5,38 @@ const baseItems = [
   { id: 4, src: './assets/4.png' },
   { id: 5, src: './assets/5.png' },
   { id: 6, src: './assets/6.png' },
-  { id: 7, src: './assets/6.png' },
-  { id: 8, src: './assets/6.png' },
-  { id: 9, src: './assets/6.png' },
-  { id: 10, src: './assets/6.png' },
+  { id: 7, src: './assets/7.png' },
+  { id: 8, src: './assets/8.png' },
+  { id: 9, src: './assets/9.png' },
+  { id: 10, src: './assets/10.png' },
 ];
 const copyItems = baseItems.map(item => ({ ...item }));
 const gameItems = copyItems.concat(copyItems);
-const pairsNum = baseItems.length;
+const pairsAmount = baseItems.length;
 let currentPick = [];
-let pairsCount = 0;
-const body = document.getElementsByTagName('body')
+let pairsMatchedCount = 0;
+const body = document.body;
 const containerEl = document.getElementById('container');
 const messageElement = document.createElement('div');
 Object.assign(messageElement.style, {
-  'font-size': '4rem',
-  'margin-bottom': '32px',
-  'color': 'red'
-})
-body[0].prepend(messageElement)
+  'font-size': '3rem',
+  'margin-bottom': '24px',
+  color: 'red',
+});
+body.prepend(messageElement);
 for (const i in gameItems) {
   const cardElement = document.createElement('div');
   cardElement.classList.add('card');
   cardElement.dataset.id = gameItems[i].id;
   cardElement.dataset.index = i;
+  cardElement.addEventListener('click', onClickHandler);
+
   const cardContentElement = document.createElement('img');
   cardContentElement.src = gameItems[i].src;
+
   const cardCoverElement = document.createElement('div');
   cardCoverElement.classList.add('card-cover');
   Object.assign(cardCoverElement.style, {
-    width: '150px',
-    height: '150px',
     'background-color': '#1c7ed6',
     display: 'flex',
     'align-items': 'center',
@@ -49,34 +50,25 @@ for (const i in gameItems) {
     'object-fit': 'cover',
   });
   cardCoverElement.innerText = +i + 1;
+
   cardElement.appendChild(cardCoverElement);
   cardElement.appendChild(cardContentElement);
   containerEl.appendChild(cardElement);
 }
 
 const cardEls = containerEl.getElementsByClassName('card');
-for (const cardEl of cardEls) {
-  cardEl.addEventListener('click', onClickHandler);
-}
+
 function onClickHandler(e) {
   const cardEl = e.currentTarget;
   const cardCoverEl = cardEl.querySelector('.card-cover');
   cardCoverEl.style.visibility = 'hidden';
-  updateCurrentPick(
-    cardEl,
-    cardEl.dataset.id,
-    cardEl.dataset.index,
-    cardCoverEl
-  );
+  updateCurrentPick(cardEl, cardEl.dataset.id, cardEl.dataset.index, cardCoverEl);
   if (isPairPicked()) {
-    if (isMatch()) {
-      const [card1, card2] = currentPick;
-      card1.card.removeEventListener('click', onClickHandler);
-      card2.card.removeEventListener('click', onClickHandler);
-      pairsCount++;
+    if (isPairMatch()) {
+      currentPick.forEach(cardData => cardData.card.removeEventListener('click', onClickHandler));
+      pairsMatchedCount++;
       if (isWin()) {
         messageElement.innerText = 'You win!!!';
-        console.log('win');
       }
     } else {
       resetCardCover();
@@ -88,22 +80,18 @@ function isPairPicked() {
   if (currentPick.length === 2) return true;
   return false;
 }
-function updateCurrentPick(card, id, index, questionMark) {
-  if (
-    currentPick.length === 0 ||
-    (currentPick.length > 0 && currentPick[0].index !== index)
-  ) {
-    currentPick.push({ card, id, index, questionMark });
+function updateCurrentPick(card, id, index, cardCover) {
+  if (currentPick.length === 0 || (currentPick.length > 0 && currentPick[0].index !== index)) {
+    currentPick.push({ card, id, index, cardCover });
   }
 }
 function resetCurrentPick() {
-  currentPick = [];
+  currentPick.length = 0;
 }
-function isMatch() {
+function isPairMatch() {
   const [card1, card2] = currentPick;
 
   if (card1.id === card2.id && card1.index !== card2.index) {
-    console.log('match');
     return true;
   }
   return false;
@@ -111,11 +99,11 @@ function isMatch() {
 function resetCardCover() {
   const [card1, card2] = currentPick;
   setTimeout(() => {
-    card1.questionMark.style.visibility = 'initial';
-    card2.questionMark.style.visibility = 'initial';
-  }, 500);
+    card1.cardCover.style.visibility = 'initial';
+    card2.cardCover.style.visibility = 'initial';
+  }, 1000);
 }
 function isWin() {
-  if (pairsCount === pairsNum) return true;
+  if (pairsMatchedCount === pairsAmount) return true;
   return false;
 }
